c9145ba2a439e6e5a8443cc4d7e60d35991e7e29
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-04-28 13:01:47 -0300

    Initial code to the Biblioteca system

--- src/lp2/lab09/Biblioteca.java
+++ src/lp2/lab09/Biblioteca.java
@@ -1,5 +1,14 @@
 package lp2.lab09;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+
+    private ArrayList<Livro> livros = new ArrayList();
+    private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+    
+    public static void main(String[] args) {
+        
+    }
 
 }
