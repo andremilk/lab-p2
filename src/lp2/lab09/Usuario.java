22252a64666fad2b5870fef2036feb829fc983f2
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-04-28 12:03:38 -0300

    Inserted blank lines on Usuario.java to adequate it's code to Java conventions

--- src/lp2/lab09/Usuario.java
+++ src/lp2/lab09/Usuario.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Usuario {
+    
     private String nome;
     private int matricula;
     private ArrayList<Livro> listaLivros;
@@ -13,30 +14,38 @@ public class Usuario {
     public static enum userType {
         ALUNO, PROFESSOR
     }
+    
     public Usuario(String nome, int matricula, int tempoMaximo) {
         super();
         this.nome = nome;
         this.matricula = matricula;
         this.tempoMaximo = tempoMaximo;
     }
+    
     public String getNome() {
         return nome;
     }
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
+    
     public int getMatricula() {
         return matricula;
     }
+    
     public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
+    
     public int getTempoMaximo() {
         return tempoMaximo;
     }
+    
     public void setTempoMaximo(int tempoMaximo) {
         this.tempoMaximo = tempoMaximo;
     }
+    
     public int getMaxLivros() {
         return maxLivros;
     }
