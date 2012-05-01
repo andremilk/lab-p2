9369c2eb3359c0f34e8f65c34f4a8f4dcb01773b
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-01 10:46:17 -0300

    Finishing javadoc on Professor

--- src/lp2/lab09/Professor.java
+++ src/lp2/lab09/Professor.java
@@ -1,5 +1,12 @@
 package lp2.lab09;
 
+
+/**
+ * Classe que modela um professor.
+ * Esta classe apenas muda o tempo maximo que um Professor pode ficar com um livro.
+ * @author dekozo
+ *
+ */
 public class Professor extends Usuario {
 
     private final static int TEMPO_MAXIMO = 180;
