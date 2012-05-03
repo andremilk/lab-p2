b16fcf1d107754de5652fc384a1bfdb77e97a24b
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-03 20:34:52 -0300

    Initial work on Disciplina class

--- src/lp2/lab10/Disciplina.java
+++ src/lp2/lab10/Disciplina.java
@@ -1,5 +1,40 @@
 package lp2.lab10;
 
+import java.util.ArrayList;
+
+enum Estado {
+    CURSANDO, CONCLUIDA
+}
+
 public abstract class Disciplina {
+    
+    private String nome;
+    private ArrayList<Prova> provas;
+    private Estado estado;
+    private int creditos;
+    
+    Disciplina(String nome) {
+        this.setNome(nome);
+    }
+    
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public Estado getEstado() {
+        return estado;
+    }
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    public abstract int getCreditos();
+    
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+    public ArrayList<Prova> getProvas() {
+        return provas;
+    }
+    
 
 }
