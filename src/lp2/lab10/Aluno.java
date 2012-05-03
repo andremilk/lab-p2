166b38c6db4ed9d7b01d0ac382df507da383f7cb
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-03 20:02:39 -0300

    Added a constructor Added two methods to add Disciplina objetcs, one to use an ArrayList and other to add a single Disciplina Added getters and setters

--- src/lp2/lab10/Aluno.java
+++ src/lp2/lab10/Aluno.java
@@ -9,4 +9,50 @@ public class Aluno {
     private String curso;
     private ArrayList<Disciplina> disciplinas;
     
+    Aluno(String nome, long matricula, String curso, ArrayList<Disciplina> disciplinas) {
+        this.setNome(nome);
+        this.setMatricula(matricula);
+        this.setCurso(curso);
+        this.addDisciplinas(disciplinas);
+    }
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public long getMatricula() {
+        return matricula;
+    }
+    
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+    
+    public String getCurso() {
+        return curso;
+    }
+    
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
+    public ArrayList<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+    
+    public void addDisciplina(Disciplina disc) {
+        this.disciplinas.add(disc);
+    }
+    
+    public void addDisciplinas(ArrayList<Disciplina> disc) {
+        for(Disciplina d: disc) {
+            this.addDisciplina(d);
+        }
+    }
+    
+    
+    
 }
