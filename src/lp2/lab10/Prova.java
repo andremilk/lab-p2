1218332cab2501634e44b5151362431b2290204d
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-03 20:17:12 -0300

    Added javadoc to the Prova class

--- src/lp2/lab10/Prova.java
+++ src/lp2/lab10/Prova.java
@@ -1,12 +1,63 @@
 package lp2.lab10;
 
+/**
+ * Classe que modela uma prova.
+ * Esta classe sera utilizada no momento de adicionar notas e calcular as medias das disciplinas
+ * @author dekozo
+ *
+ */
 public class Prova {
     
+    /**
+     * Atributo que da o peso da prova
+     */
     private double peso;
+    
+    /**
+     * Atributo que da nota a prova
+     */
     private double nota;
     
+    /**
+     * Construtor da classe Prova
+     * @param peso peso da prova
+     * @param nota nota da prova
+     */
     Prova(double peso, double nota) {
-        
+        this.setPeso(peso);
+        this.setNota(nota);
+    }
+
+    /**
+     * 
+     * @return o peso da prova
+     */
+    public double getPeso() {
+        return peso;
+    }
+
+    /**
+     * 
+     * @param peso peso a ser usado
+     */
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * 
+     * @return retorna a nota obtida na prova
+     */
+    public double getNota() {
+        return nota;
+    }
+
+    /**
+     * 
+     * @param nota nota a ser atribuida a prova
+     */
+    public void setNota(double nota) {
+        this.nota = nota;
     }
 
     
