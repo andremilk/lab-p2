dbf31e4d168be1510e128d66115ba541a1ee7467
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-03 19:55:32 -0300

    Inital work on Aluno class

---
+++ src/lp2/lab10/Aluno.java
@@ -0,0 +1,12 @@
+package lp2.lab10;
+
+import java.util.ArrayList;
+
+public class Aluno {
+
+    private String nome;
+    private long matricula;
+    private String curso;
+    private ArrayList<Disciplina> disciplinas;
+    
+}
