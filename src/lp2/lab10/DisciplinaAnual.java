87c1898b16188285124f35bd486dd1d3a3d81685
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-05 17:43:15 -0300

    Added three new classes - DisciplinaAnual - DisciplinaSemestral - DisciplinaTrimestral all of these extend Disciplina

---
+++ src/lp2/lab10/DisciplinaAnual.java
@@ -0,0 +1,22 @@
+package lp2.lab10;
+
+public class DisciplinaAnual extends Disciplina {
+
+    public DisciplinaAnual(String nome, int creditos, Estado estado) {
+        super(nome, creditos, estado);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int getCreditos() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void adicionarProva() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/lp2/lab10/DisciplinaSemestral.java
@@ -0,0 +1,22 @@
+package lp2.lab10;
+
+public class DisciplinaSemestral extends Disciplina {
+
+    public DisciplinaSemestral(String nome, int creditos, Estado estado) {
+        super(nome, creditos, estado);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int getCreditos() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void adicionarProva() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
