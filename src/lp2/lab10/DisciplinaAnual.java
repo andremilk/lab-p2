aa6ea837070f80c48d5e31b94e72efb9bc2b8d14
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-05 18:23:44 -0300

    Implemented adicionarProvas on the DisciplinaAnual class

--- src/lp2/lab10/DisciplinaAnual.java
+++ src/lp2/lab10/DisciplinaAnual.java
@@ -2,6 +2,8 @@ package lp2.lab10;
 
 public class DisciplinaAnual extends Disciplina {
 
+    private final int MAX_PROVAS = 6;
+
     public DisciplinaAnual(String nome, Estado estado) {
         super(nome, estado);
         // TODO Auto-generated constructor stub
@@ -14,9 +16,15 @@ public class DisciplinaAnual extends Disciplina {
     }
 
     @Override
-    public void adicionarProva() {
-        // TODO Auto-generated method stub
-
+    public void adicionarProva() throws Exception {
+        if(this.getProvas().size() >= MAX_PROVAS)
+            return;
+        int tamLista = this.getProvas().size();
+        /**
+         * As 5 primeiras provas tem peso 1 (formam uma media aritmetica que tera peso 6)
+         * a ultima prova tem peso 4
+         */
+        this.getProvas().add(new Prova((tamLista <= 5 ? 1: 4), DataHandler.entradaNota()));
     }
 
 }
