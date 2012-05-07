e7e3812fc690db346ab8d5b0cac7a1670fec5761
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-06 21:15:14 -0300

    Added an application class named GiraHistorico

---
+++ src/lp2/lab10/GiraHistorico.java
@@ -0,0 +1,13 @@
+package lp2.lab10;
+
+public class GiraHistorico {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
