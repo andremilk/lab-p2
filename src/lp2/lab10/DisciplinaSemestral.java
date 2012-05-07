5328b2175299945e416c738fbd73fc9a33468dba
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-06 21:02:48 -0300

    Finished javadoc and added a constant to represent the credits

--- src/lp2/lab10/DisciplinaSemestral.java
+++ src/lp2/lab10/DisciplinaSemestral.java
@@ -3,16 +3,28 @@ package lp2.lab10;
 public class DisciplinaSemestral extends Disciplina {
 
     private final int MAX_PROVAS = 3;
+    private final int CREDITOS = 60;
 
-    public DisciplinaSemestral(String nome, Estado estado) {
+    /**
+     * Construtor da classe DisciplinaSemestral
+     * @param nome nome da disciplina
+     */
+    public DisciplinaSemestral(String nome) {
         super(nome, Estado.CURSANDO);
     }
 
+    /**
+     * Retorna o numero de creditos da disciplina semestral, 60 horas
+     */
     @Override
     public int getCreditos() {
-        return 60;
+        return CREDITOS;
     }
 
+    /**
+     * Adiciona provas (maximo de 3 provas)
+     * @throws Exception Excecao lancada caso a nota da prova seja invalida
+     */
     @Override
     public void adicionarProva() throws Exception {
         int tamLista = this.getProvas().size();
@@ -22,6 +34,9 @@ public class DisciplinaSemestral extends Disciplina {
         this.getProvas().add(new Prova(1, DataHandler.entradaNota(message)));
     }
 
+    /**
+     * Gera a media parcial da disciplina
+     */
     @Override
     public void gerarParcial() {
         double produtos = 0;
