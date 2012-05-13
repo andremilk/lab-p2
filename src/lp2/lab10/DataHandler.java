761d1eab842b4e0bdbb782107f1fbfb26b482204
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-05-13 19:58:25 -0300

    Using now two Scanners, one for numbers and other for Strings

--- src/lp2/lab10/DataHandler.java
+++ src/lp2/lab10/DataHandler.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 
 public class DataHandler {
     
-    private static Scanner entrada = new Scanner(System.in);
+    private static Scanner entradaString = new Scanner(System.in);
+    private static Scanner entradaNum = new Scanner(System.in);
 
     public static double entradaNota(String message) {
         System.out.println(message);
-        return entrada.nextDouble(); 
+        return entradaNum.nextDouble(); 
     }
         
     public static Aluno introMenu() {
         System.out.println("Bem vindo ao sistema GiraHistorico");
         
         System.out.println("Digite o seu nome: ");
-        String nome = entrada.nextLine();
-        
-        System.out.println("Digite sua matricula: ");
-        long matricula = entrada.nextLong();
+        String nome = entradaString.nextLine();
         
         System.out.println("Digite o seu curso: ");
-        String curso = entrada.nextLine();
+        
+        String curso = entradaString.nextLine();
+        
+        System.out.println("Digite sua matricula: ");
+        long matricula = entradaNum.nextLong();
+        
+       
+        System.out.println("O CURSO FOI " + curso);
         
         return new Aluno(nome, matricula, curso);
     }
@@ -36,16 +41,16 @@ public class DataHandler {
         System.out.println("5) Cadastrar prova final");
         System.out.println("6) Visualizar histórico");
         System.out.println("0) Sair");
-        return entrada.nextInt();
+        return entradaNum.nextInt();
     }
     
     public static int escolherDisciplina(Aluno aluno) {
         ArrayList<Disciplina> disciplinas = aluno.getListaStatus(Estado.CURSANDO);
-        for(int i = 0; i <= disciplinas.size(); i++)
+        for(int i = 0; i < disciplinas.size(); i++)
             System.out.println((i + 1) + ")" + disciplinas.get(i));
         System.out.println("Escolha a disciplina");
         
-        return aluno.getDisciplinas().indexOf(disciplinas.get(entrada.nextInt()));
+        return aluno.getDisciplinas().indexOf(disciplinas.get(entradaNum.nextInt() - 1));
     }
     
     public static Disciplina addDisciplinaMenu() {
@@ -54,14 +59,14 @@ public class DataHandler {
         do {
             System.out.println("Escolha o tipo de disciplina\n1) Trimestral\n2)Semestral\n3)Anual");
             System.out.println("Disciplina: ");
-            tipo = entrada.nextInt();
+            tipo = entradaNum.nextInt();
         } while(tipo > 3 || tipo < 1);
         
         System.out.println("Nome da disciplina :");
-        String nome = entrada.nextLine();
+        String nome = entradaString.nextLine();
         
         System.out.println("Periodo: ");
-        int periodo = entrada.nextInt();
+        int periodo = entradaString.nextInt();
         switch(tipo) {
             case 1:
                 return new DisciplinaTrimestral(nome, periodo);
